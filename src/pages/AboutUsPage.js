2b2057cd4b38e481abb89f9af5079b33270143f6
Sachin Yadav <sachin.y4dav@gmail.com>
2022-12-03 10:56:09 +0530

    Initial commit

---
+++ src/pages/AboutUsPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AboutUsPage = () => {
+  return <h1>AboutUsPage</h1>
+}
+
+export default AboutUsPage
---
+++ src/pages/CampaignPage.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const CampaignPage = () => {
+  return <h1>CampaignPage</h1>
+}
+
+export default CampaignPage
